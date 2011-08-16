2d83c06b9abd5fc29ac9a40ae206571a942c78c1
James Gifford <james@jamesrgifford.com>
2011-08-16 14:28:23 -0400

    Fixed the sysadmin script

--- base-sysadmin.sh
+++ base-sysadmin.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-sudo apt-get install htop vim emacs git-core bazaar apt-add-repository p7zip-full
+sudo apt-get install htop vim emacs git-core bzr python-software-properties p7zip-full
